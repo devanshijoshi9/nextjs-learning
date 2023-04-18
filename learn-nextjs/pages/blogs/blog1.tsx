4519fa0a50f35da4bd9f169a6c409de6f86a93f8
Devanshi Joshi <86941664+devanshijoshi9@users.noreply.github.com>
2023-04-18 16:51:05 +0530

    Feat: Nested file based routing

--- learn-nextjs/pages/blogs/blog1.tsx
+++ learn-nextjs/pages/blogs/blog1.tsx
@@ -3,11 +3,11 @@ import { Inter } from 'next/font/google'
 
 const inter = Inter({ subsets: ['latin'] })
 
-export default function About() {
+export default function Blog1() {
     return (
         <main className="flex min-h-screen flex-col items-center justify-between p-24">
 			<div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
-				<div>About Page</div>
+				<div>Blog Page 1</div>
 			</div>
 
 			<div className="relative flex place-items-center before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700/10 after:dark:from-sky-900 after:dark:via-[#0141ff]/40 before:lg:h-[360px]">
