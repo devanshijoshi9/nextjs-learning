cb99e95d11d92022a756d6bf410df0261acf1c28
Devanshi Joshi <86941664+devanshijoshi9@users.noreply.github.com>
2023-04-24 12:10:19 +0530

    Feat: Use head component

--- learn-nextjs/pages/home.tsx
+++ learn-nextjs/pages/home.tsx
@@ -2,12 +2,16 @@ import Image from 'next/image'
 import { Inter } from 'next/font/google'
 import Navbar from '@/components/Navbar';
 import styles from '@/styles/home.module.css';
+import Head from 'next/head';
 
 const inter = Inter({ subsets: ['latin'] })
 
 export default function Home() {
     return (
         <main className="flex min-h-screen flex-col items-center justify-between p-24">
+			<Head>
+				<title>Home Page</title>
+			</Head>
 			<div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
 				<span className={styles.home_main}>
 					Home Page
--- learn-nextjs/pages/index.tsx
+++ learn-nextjs/pages/index.tsx
@@ -1,6 +1,6 @@
 import Image from 'next/image'
 import { Inter } from 'next/font/google'
-import Link from 'next/link';
+import Head from 'next/head';
 import Navbar from '@/components/Navbar';
 
 const inter = Inter({ subsets: ['latin'] })
@@ -8,6 +8,9 @@ const inter = Inter({ subsets: ['latin'] })
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
+      <Head>
+        <title>Index Page</title>
+      </Head>
       <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
         <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
             <a
